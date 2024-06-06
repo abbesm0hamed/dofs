b5718e4ba8260fb1c3ee097c185ac880f461d5c0
abbesm0hamed <abbesmohamed717@gmail.com>
2024-06-07 00:21:17 +0100

    i3 update

--- .config/scripts/autostart.sh
+++ .config/scripts/autostart.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 function run {
-	if ! pgrep $1; then
-		$@ &
-	fi
+  if ! pgrep $1; then
+    $@ &
+  fi
 }
 
 run xrandr --output HDMI-1-0 --primary --mode 1920x1080 --pos 1920x0 --rate 170 --rotate normal --output HDMI-2-0 --off
 # xrandr --output DP-1 --off --output DP-2 --mode 1920x1080 --pos 1920x415 --rotate normal --output DP-3 --primary --mode 1920x1080 --pos 0x884 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 1920x1495 --rotate normal
 #
 # compositor
-run killall picom
-while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
-run picom --config ~/.config/picom/picom.conf --vsync &
+# run killall picom
+# while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+# run picom --config ~/.config/picom/picom.conf --vsync &
 
-run kill polybar
+run killall polybar
 run ~/.config/i3/polybar/launch_polybar.sh
 
 # run ~/.config/eww/launch.sh
@@ -23,17 +23,17 @@ run ~/.config/i3/polybar/launch_polybar.sh
 #autorandr horizontal
 #run caffeine
 run autotiling
-run dunst
+# run dunst
 run pamac-tray
 run variety
 run xfce4-power-manager
 run blueberry-tray
 run blueman-applet
 run nm-applet
-run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+# run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 run numlockx on
 run volumeicon
-run flameshot
+# run flameshot
 
 # run conky -c $HOME/.config/conky/conky.conf
 #you can set wallpapers in themes as well
