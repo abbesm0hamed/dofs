c0b08a8c0cea48b271c84c38dc72df0fbc384a7a
abbesm0hamed <abbesmohamed717@gmail.com>
2024-11-01 10:08:26 +0100

    fix(wezterm): tmux + wezterm colorscheme fix

--- .config/nvim/lua/abbes/config/options.lua
+++ .config/nvim/lua/abbes/config/options.lua
@@ -26,8 +26,9 @@ opt.guicursor = "n-v-c-sm:block,i-ci-ve:blinkon1"
 -- turn on termguicolors for nightfly colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
--- opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+
+opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
@@ -41,33 +42,3 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
-
--- status line config
-opt.laststatus = 2   -- Always show statusline
-opt.showmode = false -- Don't show mode in command line
--- Function to get relative file path
-local function relative_path()
-  local full_path = vim.fn.expand("%:p")
-  local cwd = vim.fn.getcwd()
-  local rel_path = vim.fn.fnamemodify(full_path, ":~:.")
-  if rel_path == "" then
-    return "[No Name]"
-  end
-  return rel_path
-end
--- Set custom statusline
-opt.statusline = table.concat({
-  " %{%v:lua.require'nvim-web-devicons'.get_icon(expand('%:t'))%} ", -- File icon (if you have nvim-web-devicons)
-  "%{%v:lua.relative_path()%}",                                      -- Relative path
-  "%m",                                                              -- Modified flag
-  "%r",                                                              -- Readonly flag
-  "%h",                                                              -- Help file flag
-  "%=",                                                              -- Right align
-  "%y",                                                              -- File type
-  -- " %{&fileencoding?&fileencoding:&encoding}",                       -- File encoding
-  -- " [%{&fileformat}]",                                               -- File format
-  " %p%%",   -- Percentage through file
-  " %l:%c ", -- Line and column
-})
--- Make the relative_path function available to statusline
-_G.relative_path = relative_path
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,49 +1,41 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
+local kanagwa_theme = require("themes.kanagwa")
 local config = {}
 
 -- Set the base font
--- config.font = wezterm.font("JetBrains Mono Nerd Font", { italic = false })
 config.font = wezterm.font("FiraCode Nerd Font", { italic = false })
 
 -- Font rules for italic and bold italic
 config.font_rules = {
   {
     italic = true,
-    font = wezterm.font("Victor Mono Semibold", { italic = true }), -- Victor Mono Italic for italics
+    font = wezterm.font("Victor Mono Semibold", { italic = true }),
   },
   {
     italic = true,
-    intensity = "Bold",                                                     -- Use intensity instead of bold
-    font = wezterm.font("Victor Mono", { italic = true, weight = "Bold" }), -- Victor Mono Bold Italic for bold italics
+    intensity = "Bold",
+    font = wezterm.font("Victor Mono", { italic = true, weight = "Bold" }),
   },
 }
 
--- Other configurations
-config.force_reverse_video_cursor = true
-config.colors = {
-  foreground = "#dcd7ba",
-  background = "#1f1f28",
-  cursor_bg = "#c8c093",
-  cursor_fg = "#c8c093",
-  cursor_border = "#c8c093",
-  selection_fg = "#c8c093",
-  selection_bg = "#2d4f67",
-  scrollbar_thumb = "#16161d",
-  split = "#16161d",
-  ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-  brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-  indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
-}
+-- Kanagawa color scheme
+config.colors = kanagwa_theme
 
+-- Terminal configuration
+config.force_reverse_video_cursor = true
 config.tab_bar_at_bottom = true
-config.enable_tab_bar = true
+config.enable_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = true
 config.font_size = 12
+
+-- Inactive pane configuration
 config.inactive_pane_hsb = {
   saturation = 0.7,
   brightness = 0.6,
 }
+
+-- Enable true color support
+config.enable_kitty_graphics = true
 
 return config
