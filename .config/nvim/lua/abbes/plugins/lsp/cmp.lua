9ec8d8cba7dc78a153172684d47590bc9b0a45fd
abbesm0hamed <abbesmohamed717@gmail.com>
2025-04-02 10:20:51 +0100

    update(nvim): cmp.lua ( blink.cmp update + vim-dadbod db ui )

--- .config/nvim/lua/abbes/plugins/lsp/cmp.lua
+++ .config/nvim/lua/abbes/plugins/lsp/cmp.lua
@@ -8,59 +8,185 @@ local sourceIcons = {
   emmet_ls = "󰌝",
 }
 
+local trigger_text = ";" -- Define trigger_text for snippets
+
 return {
   {
     "saghen/blink.cmp",
-    -- optional: provides snippets for the snippet source
     dependencies = {
+      "moyiz/blink-emoji.nvim",
+      "Kaiser-Yang/blink-cmp-dictionary",
       "rafamadriz/friendly-snippets",
       {
         "saghen/blink.compat",
-        optional = true, -- make optional so it's only enabled if any extras need it
+        optional = true,
         opts = {},
         version = not vim.g.lazyvim_blink_main and "*",
       },
     },
-
-    -- use a release tag to download pre-built binaries
     version = "*",
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
+    opts = function(_, opts)
+      opts.enabled = function()
+        local filetype = vim.bo[0].filetype
+        if filetype == "TelescopePrompt" or filetype == "minifiles" or filetype == "snacks_picker_input" then
+          return false
+        end
+        return true
+      end
 
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      -- 'default' for mappings similar to built-in completion
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-      -- See the full "keymap" documentation for information on defining your own keymap.
-      keymap = {
-        preset = "enter",
-        ["<Tab>"] = { "select_next" },
-        ["<S-Tab>"] = { "select_prev" },
-        ["<C-j>"] = { "select_next" },
-        ["<C-k>"] = { "select_prev" },
-        ["<CR>"] = { "accept", "fallback" },
-      },
+      opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
+        default = { "lsp", "path", "snippets", "buffer", "dadbod", "emoji", "dictionary" },
+        providers = {
+          lsp = {
+            name = "lsp",
+            enabled = true,
+            module = "blink.cmp.sources.lsp",
+            min_keyword_length = 1, -- Trigger after 1 character
+            score_offset = 90,
+          },
+          path = {
+            name = "Path",
+            module = "blink.cmp.sources.path",
+            score_offset = 25,
+            fallbacks = { "snippets", "buffer" },
+            opts = {
+              trailing_slash = false,
+              label_trailing_slash = true,
+              get_cwd = function(context)
+                return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+              end,
+              show_hidden_files_by_default = true,
+            },
+          },
+          buffer = {
+            name = "Buffer",
+            enabled = true,
+            max_items = 3,
+            module = "blink.cmp.sources.buffer",
+            min_keyword_length = 1,
+            score_offset = 15,
+          },
+          snippets = {
+            name = "snippets",
+            enabled = true,
+            max_items = 15,
+            min_keyword_length = 1,
+            module = "blink.cmp.sources.snippets",
+            score_offset = 85,
+            should_show_items = function()
+              local col = vim.api.nvim_win_get_cursor(0)[2]
+              local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
+              return before_cursor:match(trigger_text .. "%w*$") ~= nil
+            end,
+            transform_items = function(_, items)
+              local line = vim.api.nvim_get_current_line()
+              local col = vim.api.nvim_win_get_cursor(0)[2]
+              local before_cursor = line:sub(1, col)
+              local start_pos, end_pos = before_cursor:find(trigger_text .. "[^" .. trigger_text .. "]*$")
+              if start_pos then
+                for _, item in ipairs(items) do
+                  if not item.trigger_text_modified then
+                    item.trigger_text_modified = true
+                    item.textEdit = {
+                      newText = item.insertText or item.label,
+                      range = {
+                        start = { line = vim.fn.line(".") - 1, character = start_pos - 1 },
+                        ["end"] = { line = vim.fn.line(".") - 1, character = end_pos },
+                      },
+                    }
+                  end
+                end
+              end
+              return items
+            end,
+          },
+          dadbod = {
+            name = "Dadbod",
+            module = "vim_dadbod_completion.blink",
+            min_keyword_length = 1,
+            score_offset = 85,
+          },
+          emoji = {
+            module = "blink-emoji",
+            name = "Emoji",
+            score_offset = 93,
+            min_keyword_length = 1,
+            opts = { insert = true },
+          },
+          dictionary = {
+            module = "blink-cmp-dictionary",
+            name = "Dict",
+            score_offset = 20,
+            enabled = true,
+            max_items = 8,
+            min_keyword_length = 2,
+            opts = {
+              dictionary_directories = { vim.fn.expand("~/dofs/dictionaries") },
+            },
+          },
+        },
+      })
 
-      appearance = {
-        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- Useful for when your theme doesn't support blink.cmp
-        -- Will be removed in a future release
-        use_nvim_cmp_as_default = true,
-        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = "mono",
-      },
+      opts.cmdline = {
+        enabled = true,
+      }
 
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-      },
-    },
+      opts.completion = {
+        menu = {
+          border = "none", 
+        },
+        documentation = {
+          auto_show = false, 
+          window = {
+            border = "none",
+          },
+        },
+      }
+
+      opts.snippets = {
+        preset = "luasnip",
+      }
+        
+      opts.keymap = {
+        preset = "default",
+        ["<Tab>"] = { "accept", "snippet_forward", "fallback" }, 
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<C-j>"] = { "select_next", "fallback" },
+        ["<S-k>"] = { "scroll_documentation_up", "fallback" },
+        ["<S-j>"] = { "scroll_documentation_down", "fallback" },
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" }, 
+        ["<C-e>"] = { "hide", "fallback" },
+      }
+
+      return opts
+    end,
     opts_extend = { "sources.default" },
+  },
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod" },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" } },
+    },
+    keys = {
+      { "<leader>d", "<cmd>DBUIToggle<CR>", desc = "Toggle DBUI" },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      vim.g.db_ui_show_help = 0
+      vim.g.db_ui_win_position = "right"
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_use_nvim_notify = 1
+      vim.g.db_ui_save_location = "~/Library/Mobile Documents/com~apple~CloudDocs/db-ui"
+      vim.g.db_ui_hide_schemas = { "pg_toast_temp.*" }
+    end,
   },
 }
