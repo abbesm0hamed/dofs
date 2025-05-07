b7e27a0f8299805cd281bbf86492e006d2aa3060
abbesm0hamed <abbesmohamed717@gmail.com>
2025-05-07 14:49:31 +0100

    update(nvim): mason update and kanso colorscheme added

--- .config/nvim/lua/abbes/plugins/colorscheme.lua
+++ .config/nvim/lua/abbes/plugins/colorscheme.lua
@@ -1,40 +1,76 @@
 return {
-  -- best
+  -- kanagawa evolution kanso
   {
-    "rebelot/kanagawa.nvim",
+    "webhooked/kanso.nvim",
     lazy = true,
     event = "VimEnter",
     priority = 1000,
     config = function()
-      require("kanagawa").setup({
+      require("kanso").setup({
         compile = false, -- enable compiling the colorscheme
         undercurl = true, -- enable undercurls
         commentStyle = { italic = true },
-        functionStyle = { italic = false, bold = false },
-        keywordStyle = { italic = false },
-        statementStyle = { bold = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = {},
         typeStyle = {},
+        disableItalics = false,
         transparent = false, -- do not set background color
         dimInactive = false, -- dim inactive window `:h hl-NormalNC`
         terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = {
-          -- add/modify theme and palette colors
+        colors = { -- add/modify theme and palette colors
           palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+          theme = { zen = {}, pearl = {}, ink = {}, all = {} },
         },
         overrides = function(colors) -- add/modify highlights
           return {}
         end,
-        theme = "wave", -- Load "wave" theme when 'background' option is not set
+        theme = "zen", -- Load "zen" theme
         background = { -- map the value of 'background' option to a theme
-          dark = "wave", -- try "dragon" !
-          light = "lotus",
+          dark = "zen", -- try "ink" !
+          light = "pearl",
         },
       })
 
-      vim.cmd("colorscheme kanagawa")
+      vim.cmd("colorscheme kanso")
     end,
   },
+  -- best
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  --   lazy = true,
+  --   event = "VimEnter",
+  --   priority = 1000,
+  --   config = function()
+  --     require("kanagawa").setup({
+  --       compile = false, -- enable compiling the colorscheme
+  --       undercurl = true, -- enable undercurls
+  --       commentStyle = { italic = true },
+  --       functionStyle = { italic = false, bold = false },
+  --       keywordStyle = { italic = false },
+  --       statementStyle = { bold = true },
+  --       typeStyle = {},
+  --       transparent = false, -- do not set background color
+  --       dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+  --       terminalColors = true, -- define vim.g.terminal_color_{0,17}
+  --       colors = {
+  --         -- add/modify theme and palette colors
+  --         palette = {},
+  --         theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+  --       },
+  --       overrides = function(colors) -- add/modify highlights
+  --         return {}
+  --       end,
+  --       theme = "wave", -- Load "wave" theme when 'background' option is not set
+  --       background = { -- map the value of 'background' option to a theme
+  --         dark = "wave", -- try "dragon" !
+  --         light = "lotus",
+  --       },
+  --     })
+  --
+  --     vim.cmd("colorscheme kanagawa")
+  --   end,
+  -- },
   -- black metal theme
   -- {
   --   "metalelf0/black-metal-theme-neovim",
