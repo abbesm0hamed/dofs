1f6c2dad1028548f3b17d85b3a824ff14e70bef1
abbesm0hamed <abbesmohamed717@gmail.com>
2025-12-17 00:00:09 +0100

    update: minor power menu ui

--- .config/waybar/scripts/power-menu.sh
+++ .config/waybar/scripts/power-menu.sh
@@ -2,7 +2,7 @@
 
 options="󰌾 Lock\n󰗽 Logout\n󰤄 Suspend\n󰜉 Reboot\n󰐥 Shutdown"
 
-selected=$(echo -e "$options" | fuzzel --dmenu --prompt="󰐥 Power: " --width=32 --lines=5)
+selected=$(echo -e "$options" | fuzzel --dmenu --prompt="󰐥 Power: " --width=25 --lines=5 --line-height=15)
 
 case $selected in
     "󰌾 Lock")
