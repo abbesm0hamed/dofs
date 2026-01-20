279bae4e736dac4564ddeaedf9beea6206201fab
abbesm0hamed <abbesmohamed717@gmail.com>
2026-01-20 18:04:35 +0100

    refactor(waybar): better organized modules and cleaner styling

--- home/.config/waybar/scripts/docker-event-status.sh
+++ home/.config/waybar/scripts/docker-event-status.sh
@@ -32,28 +32,4 @@ get_status() {
     fi
 }
 
-while true; do
-    get_status
-
-    if ! docker_cli_available; then
-        sleep 10
-        continue
-    fi
-
-    if ! docker_daemon_available; then
-        sleep 5
-        continue
-    fi
-
-    # Listen for container events (start, die, stop, etc.)
-    # We use stdbuf to ensure line-buffered output from docker events
-    stdbuf -oL docker events --filter "type=container" --format "{{.Action}}" 2>/dev/null | while read -r event; do
-        case "$event" in
-            start|die|stop|kill|pause|unpause|restart)
-                get_status
-                ;;
-        esac
-    done
-
-    sleep 2
-done
+get_status
