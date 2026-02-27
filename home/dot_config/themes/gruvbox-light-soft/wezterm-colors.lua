775fb9e6f236ee6d1fadc200a408742b5581790e
abbesm0hamed <abbesmohamed717@gmail.com>
2026-02-27 14:34:34 +0100

    feat: new light theme ( gruvbox light soft )...

---
+++ home/dot_config/themes/gruvbox-light-soft/wezterm-colors.lua
@@ -0,0 +1,35 @@
+local M = {}
+
+M.colors = {
+  foreground = "#3c3836",
+  background = "#f2e5bc",
+  cursor_bg = "#665c54",
+  cursor_fg = "#f2e5bc",
+  cursor_border = "#665c54",
+  selection_fg = "#3c3836",
+  selection_bg = "#d5c4a1",
+  scrollbar_thumb = "#bdae93",
+  split = "#d5c4a1",
+  ansi = {
+    "#ebdbb2",
+    "#cc241d",
+    "#98971a",
+    "#d79921",
+    "#458588",
+    "#b16286",
+    "#689d6a",
+    "#504945",
+  },
+  brights = {
+    "#bdae93",
+    "#fb4934",
+    "#b8bb26",
+    "#fabd2f",
+    "#83a598",
+    "#d3869b",
+    "#8ec07c",
+    "#282828",
+  },
+}
+
+return M
